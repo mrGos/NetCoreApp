93f65f2fcb1b8793d8c918da771e01ed4e3ad52d
mrGos <hoangkhang0235@gmail.com>
2019-02-16 18:44:19 +0700

    update repository

---
+++ CoreApp.Data.EF/IUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreApp.Infrastructure.Interfaces;
+
+namespace CoreApp.Data.EF
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public EFUnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
